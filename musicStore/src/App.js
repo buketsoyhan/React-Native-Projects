0cffafa580bb6ea07342be542a69b43958e8c8b0
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-09-19 16:30:08 +0300

    Added Search Bar for song title

--- musicStore/src/App.js
+++ musicStore/src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import music_data from './music_data.json';
 import SongCard from './components/SongCard';
+import SearchBar from './components/SearchBar';
 
 function App() {
+  const [list,setList]= useState(music_data)
+
   const renderSong = ({item}) => <SongCard song={item} />;
 
   const renderSeperator = () => <View style={styles.seperator} />;
 
+  const handleSearch = text =>{
+    const filteredList= music_data.filter(song=>{
+      const searchedText = text.toLowerCase();
+      const currentTitle= song.title.toLowerCase();
+
+      return currentTitle.indexOf(searchedText)>-1;
+    })
+
+    setList(filteredList)
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <FlatList
-          keyExtractor={item => item.id}
-          data={music_data}
-          renderItem={renderSong}
-          ItemSeparatorComponent={renderSeperator}
-        />
-      </View>
+      <SearchBar onSearch={handleSearch} />
+      <FlatList
+        keyExtractor={item => item.id}
+        data={list}
+        renderItem={renderSong}
+        ItemSeparatorComponent={renderSeperator}
+      />
     </SafeAreaView>
   );
 }
